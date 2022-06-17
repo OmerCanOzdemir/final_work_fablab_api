3f571c3e2125c8da577e1edf7a0b9f621aede906
Omer Can Özdemir <omer.can.ozdemir@student.ehb.be>
2022-06-17 02:17:07 +0200

    feat: setup service and controller testing

---
+++ Tests/ControllerTesting/ControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using api.Controllers;
+using business_logic.services.interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.ControllerTesting
+{
+    [TestClass]
+    public class ControllerTests
+    {
+
+        private ProjectController _projectController;
+
+        private readonly Mock<IProjectService> _projectServiceMock = new Mock<IProjectService>();
+
+
+        private UserController _userController;
+        private readonly Mock<IUserService> _userServiceMock = new Mock<IUserService>();
+
+        private EducationController _educationController;
+        private readonly Mock<IEducationService> _educationServiceMock = new Mock<IEducationService>();
+
+        private CategoryController _categoryController;
+        private readonly Mock<ICategoryService> _categoryServiceMock = new Mock<ICategoryService>();
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _projectController = new ProjectController(_projectServiceMock.Object);
+            _userController = new UserController(_userServiceMock.Object);
+            _educationController = new EducationController(_educationServiceMock.Object);
+            _categoryController = new CategoryController(_categoryServiceMock.Object);
+        }
+    }
+}
---
+++ Tests/ServiceTesting/ServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using business_logic.services;
+using business_logic.services.interfaces;
+using data.repositories.interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.ServiceTesting
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        private IUserService _userServices;
+        private readonly Mock<IUserRepository> _userServicesMock = new Mock<IUserRepository>();
+
+        private IProjectService _projectServices;
+        private readonly Mock<IProjectRepository> _projectServicesMock = new Mock<IProjectRepository>();
+
+        private IEducationService _educationServices;
+        private readonly Mock<IEducationRepository> _educationServicesMock = new Mock<IEducationRepository>();
+
+        private ICategoryService _categoryServices;
+        private readonly Mock<ICategoryRepository> _categoryServicesMock = new Mock<ICategoryRepository>();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _userServices = new UserService(_userServicesMock.Object);
+            _projectServices = new ProjectService(_projectServicesMock.Object);
+            _educationServices = new EducationService(_educationServicesMock.Object);
+            _categoryServices = new CategoryService(_categoryServicesMock.Object);
+
+        }
+
+
+
+    }
+}
